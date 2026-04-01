d0b72fd8221bafd40412373326d058a0f7cb4743
Truong Gia Huy <huy.truong@devsamurai.com>
2026-04-01 12:13:22 +0700

    PW-03:fix testing

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
+    "^@/api/apiClient$": "<rootDir>/src/api/__mocks__/apiClient.ts",
     "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg|webp)$": "<rootDir>/src/__mocks__/fileMock.ts",
@@ -14,6 +15,12 @@ const config: Config = {
   },
   testMatch: ["**/__tests__/**/*.test.(ts|tsx)"],
   collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/main.tsx"],
+  globals: {
+    "ts-jest": {
+      tsconfig: { jsx: "react-jsx" },
+      diagnostics: false,
+    },
+  },
 };
 
 export default config;
